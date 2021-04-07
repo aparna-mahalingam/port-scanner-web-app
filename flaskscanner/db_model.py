d79e1b1bac1210967745657b554c46445f589785
aparna-mahalingam <82092430+aparna-mahalingam@users.noreply.github.com>
2021-04-07 13:15:57 -0700

    Create db_model.py
    Specifies the schema for the database in the form of models

---
+++ flaskscanner/db_model.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from flaskscanner import db
+import logging
+
+
+
+class IPScanDetails(db.Model):
+	__tablename__ = 'ip_scan_details'
+	ip_address = db.Column(db.String(20), primary_key=True, nullable=False)
+	timestamp = db.Column(db.DateTime, primary_key=True, nullable=False, default=datetime.now)
+
+	open_ports = db.Column(db.Text, nullable=False)
+
+	def __repr__(self):
+		return f"ScanResult('{self.ip_address}', '{self.timestamp}', '{
